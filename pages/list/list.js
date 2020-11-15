1f8d51a03a9c347dfaffe489c672eb0cea443abb
shaweiwei <shamo89@126.com>
2020-11-15 20:20:18 +0800

    修改进度条样式

--- pages/list/list.js
+++ pages/list/list.js
@@ -5,7 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    "songlist": [
+      {
+        "title": "唯美",
+        "name": "Yesterday Once More",
+        "diagram": "http://091275.com/wp-content/uploads/2020/05/0000000000000000001.jpg"
+      },
+      {
+        "title": "唯美",
+        "name": "The End Of The World",
+        "diagram": "http://091275.com/wp-content/uploads/2020/11/%E7%BB%8F%E5%85%B8%E6%AD%8C%E6%9B%B2%E3%80%8AThe-End-Of-The-World%E3%80%8B%E3%80%8A%E4%B8%96%E7%95%8C%E6%9C%AB%E6%97%A5%E3%80%8B%E4%B8%AD%E8%8B%B1%E6%96%87%E5%AF%B9%E7%85%A7%E6%AD%8C%E8%AF%8D.jpg"
+      },
+      {
+        "title": "励志",
+        "name": "La luna",
+        "diagram": "http://091275.com/wp-content/uploads/2020/11/%E7%BB%8F%E5%85%B8%E6%AD%8C%E6%9B%B2%E3%80%8ALa-luna%E3%80%8B%E3%80%8A%E6%9C%88%E5%85%89%E5%A5%B3%E7%A5%9E%E3%80%8B%E4%B8%AD%E8%8B%B1%E6%96%87%E5%AF%B9%E7%85%A7%E6%AD%8C%E8%AF%8D.jpg"
+      },
+      {
+        "title": "励志",
+        "name": "Tie A Yellow Ribbon Round The Old Oak Tree",
+        "diagram": "http://091275.com/wp-content/uploads/2020/10/经典歌曲《big-big-world》《大大世界》中英文对照歌词.jpg"
+      }
+    ]
   },
 
   /**
